1dcfc1a4771b26b2d692f56722bf800fa2da61f8
Austin Testut <austintestut@gmail.com>
2021-07-21 15:49:39 -0500

    added skeleton of Input form for challenge rating

---
+++ src/Input.js
@@ -0,0 +1,10 @@
+const Input = () => {
+  return (
+    <form>
+      <label>Desired CR: </label>
+      <input type="text" placeholder="Input Challenge Rating" />
+    </form>
+  );
+};
+
+export default Input;
