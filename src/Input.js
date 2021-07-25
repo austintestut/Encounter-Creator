71dd188d5915c9babdd4183ade79aaa3cc00b460
Austin Testut <austintestut@gmail.com>
2021-07-25 17:49:04 -0500

    created second form for average level and number of party members

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,18 @@
 const Input = () => {
   return (
-    <form>
-      <label>Desired CR: </label>
-      <input type="text" placeholder="Input Challenge Rating" />
-    </form>
+    <div id="input">
+      <form id="CR-form">
+        <label>Desired CR: </label>
+        <input type="text" placeholder="Input Challenge Rating" />
+      </form>
+      <span> OR </span>
+      <form id="number-form">
+        <label>Number of Party Members: </label>
+        <input type="text" placeholder="# of Members" />
+        <label> Average Level of Party Members: </label>
+        <input type="text" placeholder="Party Level" />
+      </form>
+    </div>
   );
 };
 
